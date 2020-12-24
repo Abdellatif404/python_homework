a765dc42ab15df3387e3a41785c8949580d60e32
abdellatif <you@example.com>
2020-12-24 20:10:35 +0100

    Update

--- BLACK JACK/main.py
+++ BLACK JACK/main.py
@@ -3,20 +3,18 @@ import player
 
 
 class Blackjack:
-    bets = [5, 10, 25, 50, 100]
+    bets = [5, 10, 25, 50, 100, 250, 500]
     human = player.Player('Player', bets)
-    computer = player.Player('Dealer', bets)
+    dealer = player.Player('Dealer', bets)
 
-    def draw_gameplay(self):
-        dealer_cards = self.computer.cards
+    def print_game(self):
+        dealer_cards = self.dealer.cards
         player_cards = self.human.cards
         length = len(dealer_cards)
-        print('')
-        print(f'Dealer: {dealer_cards[:length - 1]}, * = {sum(dealer_cards)}')
-        print(f' You  : {player_cards} = {sum(player_cards)}')
-        print('')
+        print(f'\nDealer: {dealer_cards[:length - 1]}, * = {sum(dealer_cards)}')
+        print(f' You  : {player_cards} = {sum(player_cards)}\n')
 
-    def pick_card(self):
+    def take_card(self):
         ace = random.randint(1, 11)
         jack = queen = king = 10
         deck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, jack, queen, king, ace]
@@ -24,59 +22,55 @@ class Blackjack:
 
     def place_bet(self):
         while True:
-            user_input = input('put your bet (5,10,25,50,100): ')
+            selected_bet = input('put a bet (5,10,25,50,100): ')
             money = self.human.money
-
-            if user_input.isdigit() and int(user_input) in self.bets and int(user_input) <= money:
-                self.human.bet = int(user_input)
+            if selected_bet.isdigit() and int(selected_bet) in self.bets and int(selected_bet) <= money:
+                self.human.bet = int(selected_bet)
                 break
             else:
                 print('Try again')
 
-    def compare(self):
-        dealer_cards = sum(self.computer.cards)
+    def compare_cards(self):
+        dealer_cards = sum(self.dealer.cards)
         player_cards = sum(self.human.cards)
         if player_cards == dealer_cards:
-            print('')
-            print('PUSH')
+            print('\nPUSH')
             return True
         elif player_cards < dealer_cards:
-            print(self.human.bet)
-            print('')
-            print('LOSE')
+            print('\nLOSE')
             return True
 
     def dealer_roll(self):
-        computer = self.computer
+        computer = self.dealer
         while True:
-            self.computer.hit(self.pick_card())
-            self.draw_gameplay()
-            if computer.check_blackjack_and_bust() or self.compare():
+            computer.hit(self.take_card())
+            self.print_game()
+            if computer.check_blackjack_and_bust() or self.compare_cards():
                 return True
             else:
                 continue
 
-    def hit_or_stand(self):
+    def actions(self):
         game_on = True
         while game_on:
-            choice = input('H for hit or S for stand:').capitalize()
-            if self.human.check_blackjack_and_bust() or self.computer.check_blackjack_and_bust():
+            choice = input('H for hit / S for stand:').capitalize()
+            if self.human.check_blackjack_and_bust() or self.dealer.check_blackjack_and_bust():
                 game_on = False
             if choice == 'H':
-                self.human.hit(self.pick_card())
-                self.draw_gameplay()
+                self.human.hit(self.take_card())
+                self.print_game()
                 if self.human.check_blackjack_and_bust():
                     game_on = False
             elif choice == 'S':
-                if self.compare() or self.dealer_roll():
+                if self.compare_cards() or self.dealer_roll():
                     game_on = False
 
     def play(self):
         self.place_bet()
-        self.computer.cards = [self.pick_card(), self.pick_card()]
-        self.human.cards = [self.pick_card(), self.pick_card()]
-        self.draw_gameplay()
-        self.hit_or_stand()
+        self.dealer.cards = [self.take_card(), self.take_card()]
+        self.human.cards = [self.take_card(), self.take_card()]
+        self.print_game()
+        self.actions()
 
 
 Blackjack().play()
